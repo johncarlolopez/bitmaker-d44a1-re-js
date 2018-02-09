73f3b65bc720b9468afde8abf6f3ec47f23fe191
John Carlo Lopez <condottiere.jc@gmail.com>
2018-02-09 13:29:09 -0500

    complete assignement

---
+++ excercise.js
@@ -0,0 +1,47 @@
+project = {
+  committee: ["Stella", "Salma", "Kai"],
+  title: "Very Important Project",
+  dueDate : "December 14, 2019",
+  id: "3284",
+  steps: [
+    {description: "conduct interviews",
+     dueDate: "August 1, 2018"
+    },
+    {description: "code of conduct",
+     dueDate: "January 1, 2018"
+    },
+    {description: "compile results",
+     dueDate: "November 10, 2018"
+    },
+    {description: "version 1",
+     dueDate: "January 15, 2019"
+    },
+    {description: "revisions",
+     dueDate: "March 30, 2019"
+    },
+    {description: "version 2",
+     dueDate: "July 12, 2019"
+    },
+    {description: "final edit",
+     dueDate: "October 1, 2019"
+    },
+    {description: "final version",
+     dueDate: "November 20, 2019"
+    },
+    {description: "Wrap up",
+     dueDate: "December 1, 2019"
+    }
+  ]
+}
+
+for (var i = 1; i < (project.steps.length + 1); i++) {
+  if (i % 3 === 0) {
+    project.steps[i - 1].committee = project.committee[2]
+  } else if (i % 2 === 0) {
+    project.steps[i - 1].committee = project.committee[1]
+  } else {
+    project.steps[i - 1].committee = project.committee[0]
+  }
+}
+
+console.log(project);
